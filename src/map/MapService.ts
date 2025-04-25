7ae53c7b4c7d6172f7acd0e76605ac63fcdd6527
tarymee <tarymee@gmail.com>
2025-04-25 14:49:16 +0800

    up

--- src/map/MapService.ts
+++ src/map/MapService.ts
@@ -22,12 +22,51 @@ class MapService {
   private isInit = false
   private initPromise: any = null
 
+  get lbssetting(): ILbsSettingData | undefined {
+    return tenantSetting.get('lbssetting')
+    // const lbssetting: any = {
+    //   enable: '1',
+    //   setting: {
+    //     type: '',
+    //     key: {
+    //       web: '',
+    //       android: '',
+    //       ios: '',
+    //       harmony: ''
+    //     },
+    //     secretkey: {
+    //       web: '',
+    //       android: '',
+    //       ios: '',
+    //       harmony: ''
+    //     }
+    //   }
+    // }
+    // // // 高德地图公司key
+    // // lbssetting.setting.type = 'amap'
+    // // lbssetting.setting.key.web = '1993ac213d2f4675ac1bffb1b03ef1f0'
+    // // lbssetting.setting.secretkey.web = '816fe46b7b7bce145940b93c1e4818fa'
+    // // 高德地图个人key
+    // lbssetting.setting.type = 'amap'
+    // lbssetting.setting.key.web = 'e4d25fe4661a34198c4e6f79abe9afac'
+    // lbssetting.setting.secretkey.web = 'a6b674affd9a3278c68602cf7ba02fcb'
+    // // // 腾讯地图
+    // // lbssetting.setting.type = 'tencent'
+    // // lbssetting.setting.key.web = 'NHBBZ-K5LCQ-LF35M-2CTDP-E4OO7-AIBFT'
+    // // lbssetting.setting.secretkey.web = 'zowvV5I2pSxqgGb2Sgr1x62HGXbqdxT0'
+    // // // 百度地图
+    // // lbssetting.setting.type = 'baidu'
+    // // lbssetting.setting.key.web = '7r3bsPeQqJ74vsxf3EOXg7C1AM4lOWA1'
+    // // lbssetting.setting.secretkey.web = ''
+    // return lbssetting
+  }
+
   get isLbssettingEnable() {
-    return tenantSetting.get('lbssetting')?.enable === '1'
+    return this.lbssetting?.enable === '1'
   }
 
   private get setting(): ILbsSetting | undefined {
-    return tenantSetting.get('lbssetting')?.setting
+    return this.lbssetting?.setting
   }
 
   get key() {
@@ -58,34 +97,6 @@ class MapService {
   TMap: any = null
   BMap: any = null
   MapCore: any = null
-
-  // get AMap() {
-  //   if (!this.isInit || this.type !== 'amap') {
-  //     return null
-  //   }
-  //   return window.AMap
-  // }
-
-  // get TMap() {
-  //   if (!this.isInit || this.type !== 'tencent') {
-  //     return null
-  //   }
-  //   return window.TMap
-  // }
-
-  // get BMap() {
-  //   if (!this.isInit || this.type !== 'baidu') {
-  //     return null
-  //   }
-  //   return window.BMap
-  // }
-
-  // get MapCore() {
-  //   if (!this.isInit) {
-  //     return null
-  //   }
-  //   return window.BMap
-  // }
 
   async init() {
     if (this.isInit) return
@@ -134,10 +145,11 @@ class MapService {
     this.isInit = true
   }
 
-  // reset() {
-  //   this.isInit = false
-  // }
-
+  // 腾讯地图 key 配置错误
+  // 1 如果是地图 会在地图上明确文字提示 鉴权失败，请传入正确的key
+  // 2 如果是调用 api 会返回 catch 和详细的错误信息
+  // getIPLocationByTMap fail: {"status":311,"message":"key格式错误","request_id":"ccedb04fd95e4f3f9cd45cfbad729d10","id":"cbm919vjdj0"}
+  // getIPLocationByTMap fail: {"status":190,"message":"无效的key","request_id":"b6ca9d0749eb4c91a47db9412b1253ca","id":"cbm919wxo40"}
   private async initTecent() {
     await importJS(
       `https://map.qq.com/api/gljs?v=1.exp&libraries=service&key=${this.key}`,
@@ -149,6 +161,9 @@ class MapService {
     await delay(300)
   }
 
+  // 高德地图 key 配置错误
+  // 1 如果是 key 格式不对 那么没返回 promise 连 window.AMap 都拿不到 地图和都不能用
+  // 1 如果是 key 格式对 但 key 不对 有返回 window.AMap 但 api 定位调用失败 地图可以渲染空白地图
   private async initAmap() {
     return new Promise(async (resolve, reject) => {
       // 高德地图api初始化失败 没有返回reject 因此用超时机制检测
@@ -190,6 +205,8 @@ class MapService {
     })
   }
 
+  // 百度地图 key 配置错误
+  // alert以下信息: 您提供的密钥不是有效的百度LBS开放平台密钥，或此密钥未对本应用的百度地图JavaScriptAPI授权。您可以访问如下网址了解如何获取有效的密钥：http://lbsyun.baidu.com/apiconsole/key#。
   private async initBaidu() {
     return new Promise((resolve, reject) => {
       window.BMAP_INITIAL_CALLBACK = (e: any) => {
